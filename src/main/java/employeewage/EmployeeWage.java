918fbd081eaeecc4046d262048cd5832e39d9c0a
Anushka <anushkamar1102@gmail.com>
2023-11-23 20:30:16 +0530

    Refactored: Added daily-wage-calculator

--- src/main/java/employeewage/EmployeeWage.java
+++ src/main/java/employeewage/EmployeeWage.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class EmployeeWage {
 
+    private static final int WAGE_PER_HR = 20;
+    private static final int FULL_TIME_HR = 8;
+    private static final int PART_TIME_HR = 4;
 
      public static void checkAtt(){
         Random random = new Random();
@@ -25,9 +28,41 @@ public class EmployeeWage {
         }
     }
 
+    public static int checkEmpType(){
+         Random random = new Random();
+         int emp_type = random.nextInt(2);
+
+         switch (emp_type){
+             case 0:
+                 return 0;
+             case 1:
+                 return 1;
+             default:
+                 return -1;
+         }
+
+    }
+
+    public static void daily_emp_wage(int emp_type){
+
+         switch (emp_type){
+             case 0:
+                 System.out.println("Daily wage of part-time emp is: "+ PART_TIME_HR*WAGE_PER_HR);
+                 break;
+             case 1:
+                 System.out.println("Daily wage of full-time emp is: "+ FULL_TIME_HR*WAGE_PER_HR);
+                 break;
+             default:
+                 System.out.println("INVALID EMPLOYEE-TYPE");
+         }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage calculator");
 
         checkAtt();
+        int emp_type = checkEmpType();
+        daily_emp_wage(emp_type);
+
     }
 }
