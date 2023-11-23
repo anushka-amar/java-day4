dc3b06478d472de51981ba9ae7e8647f69459006
Anushka <anushkamar1102@gmail.com>
2023-11-23 20:03:25 +0530

    Refactored: added att checker

--- src/main/java/employeewage/EmployeeWage.java
+++ src/main/java/employeewage/EmployeeWage.java
@@ -1,10 +1,33 @@
 package employeewage;
 
+import java.lang.reflect.Field;
+import java.util.Random;
 import java.util.Scanner;
 
 public class EmployeeWage {
 
+
+     public static void checkAtt(){
+        Random random = new Random();
+        int att = (random.nextInt(2));
+
+        switch (att){
+            case 0:
+                System.out.println("Employee is present");
+                break;
+
+            case 1:
+                System.out.println("Employee is absent");
+                break;
+
+            default:
+                System.out.println("INVALID");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage calculator");
+
+        checkAtt();
     }
 }
