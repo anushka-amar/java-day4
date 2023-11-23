f89d9163f768060a2bd6cd0d01fca1532025d0df
Anushka <anushkamar1102@gmail.com>
2023-11-23 21:04:37 +0530

    Refactored: calculted monthly wage based on working days

--- src/main/java/employeewage/EmployeeWage.java
+++ src/main/java/employeewage/EmployeeWage.java
@@ -9,6 +9,9 @@ public class EmployeeWage {
     private static final int WAGE_PER_HR = 20;
     private static final int FULL_TIME_HR = 8;
     private static final int PART_TIME_HR = 4;
+    private static final int MAX_WORKING_HRS = 100;
+    private static final int WORKING_DAYS_PER_MONTH = 20;
+
 
      public static void checkAtt(){
         Random random = new Random();
@@ -43,18 +46,42 @@ public class EmployeeWage {
 
     }
 
-    public static void daily_emp_wage(int emp_type){
+    public static int daily_emp_wage(int emp_type){
 
          switch (emp_type){
              case 0:
-                 System.out.println("Daily wage of part-time emp is: "+ PART_TIME_HR*WAGE_PER_HR);
-                 break;
+                 int d_wage = PART_TIME_HR*WAGE_PER_HR;
+                 return d_wage;
              case 1:
-                 System.out.println("Daily wage of full-time emp is: "+ FULL_TIME_HR*WAGE_PER_HR);
-                 break;
+                 d_wage = FULL_TIME_HR*WAGE_PER_HR;
+                 return d_wage;
              default:
                  System.out.println("INVALID EMPLOYEE-TYPE");
+                 return -1;
          }
+    }
+
+    public static void monthly_wages(int emp_type){
+         int workingDays = 0, dailyWage = 0, total_monthly_wage=0;
+         while (workingDays<WORKING_DAYS_PER_MONTH){
+
+             switch (emp_type){
+                 case 0:
+                     dailyWage = daily_emp_wage(emp_type);
+                     break;
+                 case 1:
+                     dailyWage = daily_emp_wage(emp_type);
+                     break;
+                 default:
+                     System.out.println("INVALID emp_type");
+             }
+             total_monthly_wage += dailyWage;
+             workingDays++;
+         }
+
+        String msg = (emp_type == 0) ? ("monthly wage of part-time emp is " + total_monthly_wage) :
+                ("monthly wage of full-time emp is " + total_monthly_wage);
+        System.out.println(msg);
     }
 
     public static void main(String[] args) {
@@ -62,7 +89,11 @@ public class EmployeeWage {
 
         checkAtt();
         int emp_type = checkEmpType();
-        daily_emp_wage(emp_type);
+        int d_wage = daily_emp_wage(emp_type);
+        String msg = (emp_type == 0) ? ("daily wage of part-time emp is " + d_wage) :
+                ("daily wage of full-time emp is " + d_wage);
+        System.out.println(msg);
+        monthly_wages(emp_type);
 
     }
 }
