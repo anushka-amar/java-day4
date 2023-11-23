57466e9d95a84899a5ef067e197e049ee1d97e8f
Anushka <anushkamar1102@gmail.com>
2023-11-23 22:40:37 +0530

    Added: Created the starter file

---
+++ src/main/java/linecomparision/LineComparision.java
@@ -0,0 +1,8 @@
+package linecomparision;
+
+public class LineComparision {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparision Computation Program");
+    }
+}
