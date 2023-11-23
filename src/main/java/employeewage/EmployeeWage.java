5f935939f4ffbe6dcacab5c44aca127f3949c459
Anushka <anushkamar1102@gmail.com>
2023-11-23 22:22:16 +0530

    Refactored: calculating monthly wages based on max_working_daysand max_working_hrs

--- src/main/java/employeewage/EmployeeWage.java
+++ src/main/java/employeewage/EmployeeWage.java
@@ -62,8 +62,9 @@ public class EmployeeWage {
     }
 
     public static void monthly_wages(int emp_type){
-         int workingDays = 0, dailyWage = 0, total_monthly_wage=0;
-         while (workingDays<WORKING_DAYS_PER_MONTH){
+         int total_working_days = 0, total_work_hr=0, dailyWage = 0, total_monthly_wage=0;
+         while (total_working_days<WORKING_DAYS_PER_MONTH ){
+
 
              switch (emp_type){
                  case 0:
@@ -75,9 +76,17 @@ public class EmployeeWage {
                  default:
                      System.out.println("INVALID emp_type");
              }
+             total_work_hr += (emp_type == 0) ? PART_TIME_HR : FULL_TIME_HR;
+             if(total_work_hr>MAX_WORKING_HRS){
+                 total_work_hr -= FULL_TIME_HR;
+                 break;
+             }
+
              total_monthly_wage += dailyWage;
-             workingDays++;
+             total_working_days++;
          }
+
+        System.out.println("work hr is "+ total_work_hr);
 
         String msg = (emp_type == 0) ? ("monthly wage of part-time emp is " + total_monthly_wage) :
                 ("monthly wage of full-time emp is " + total_monthly_wage);
